be4843f4459d1500816dc19c4ef99a148893f098
molor824 <molormargaderdene@gmail.com>
2026-03-21 01:26:48 +0800

    Implement basic integer parsing

--- compiler/src/ast/mod.rs
+++ compiler/src/ast/mod.rs
@@ -1,12 +1,36 @@
+use num_bigint::BigUint;
+
 use crate::source::Source;
 
-fn parse_digit(source: &mut Source, radix: u32) -> Option<u32> {
-    let index = source.offset;
-    match source.next().and_then(|ch| ch.to_digit(radix)) {
-        Some(a) => Some(a),
-        None => {
-            source.offset = index;
-            None
-        }
+fn parse_digit(source: &mut Source, radix: u32, underscore: bool) -> Option<u32> {
+    if underscore { // Skip over underscore character is permitted
+        source.next_if(|ch| ch == '_');
+    }
+    source.next_and(|ch| ch.to_digit(radix))
+}
+// Every following digit after the first can have one underscore
+fn parse_integer(source: &mut Source, radix: u32) -> Option<BigUint> {
+    let Some(first_digit) = parse_digit(source, radix, false) else {
+        return None;
+    };
+    let mut number = BigUint::from(first_digit);
+    while let Some(digit) = parse_digit(source, radix, true) {
+        number = number * radix + digit;
+    }
+    Some(number)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
+    use num_bigint::BigUint;
+
+    use crate::{ast::parse_integer, source::Source};
+
+    #[test]
+    fn integer_parsing() {
+        let mut source = Source::new(Rc::new(RefCell::new("4_15_4".chars())));
+        assert_eq!(parse_integer(&mut source, 10), Some(BigUint::from(4154_u32)));
     }
 }
--- compiler/src/source.rs
+++ compiler/src/source.rs
@@ -1,6 +1,11 @@
 use std::{cell::RefCell, rc::Rc};
 
 #[derive(Clone)]
+/// Struct that handles iteration over chars and storing accumulated chars.
+/// 
+/// It's always mutably referenced in parser methods to advance.
+/// If parser method returns None, it's expected for the Source to be rolled back to the previous state by the parsing function
+/// However if it returns Err, it's expected for the Source to be at the location where the error occured.
 pub struct Source {
     pub iter: Rc<RefCell<dyn Iterator<Item = char>>>,
     pub accumulated: Rc<RefCell<String>>,
@@ -12,6 +17,26 @@ impl Source {
             iter,
             accumulated: Rc::new(RefCell::new(String::new())),
             offset: 0,
+        }
+    }
+    pub fn next_if(&mut self, condition: impl FnOnce(char) -> bool) -> Option<char> {
+        let offset = self.offset;
+        match self.next() {
+            Some(ch) if condition(ch) => Some(ch),
+            _ => {
+                self.offset = offset;
+                None
+            }
+        }
+    }
+    pub fn next_and<U>(&mut self, next: impl FnOnce(char) -> Option<U>) -> Option<U> {
+        let offset = self.offset;
+        match self.next().and_then(next) {
+            Some(v) => Some(v),
+            _ => {
+                self.offset = offset;
+                None
+            }
         }
     }
 }
